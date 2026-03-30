45c36944307d2a68fcb0ff2f0c3b285d2cdb3f26
josiasdev <josiasmartins098@gmail.com>
2026-03-30 15:49:26 -0300

    feat: organização das rotas no angular.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'cards', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'cards', 
+    loadComponent: () => import('./components/card-list/card-list').then(c => c.CardList)
+  },
+  { 
+    path: 'cards/new', 
+    loadComponent: () => import('./components/card-form/card-form').then(c => c.CardForm)
+  },
+  { 
+    path: 'cards/:id/edit', 
+    loadComponent: () => import('./components/card-form/card-form').then(c => c.CardForm)
+  },
+  { 
+    path: '**', 
+    redirectTo: 'cards' 
+  }
+];
